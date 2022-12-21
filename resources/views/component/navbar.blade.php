9f5570bcb5936cb21c559df6f47ba0212f8c5f59
Ignatius Filbert Sevilen <sevilenfilbert@gmail.com>
2022-12-21 21:08:20 +0700

    navbar

---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/storage/css/main.css">
+    <link rel="stylesheet" href="/storage/css/navbar.css">
+    <title>Navbar</title>
+</head>
+<body>
+    <div class="navbarContainer">
+        <div class="navbarLeft">
+            <a href="/" class="navbarLeft">
+                
+                <button class="navbarTitleButton">
+                    <img src="/storage/image/websiteicon/logo.png" class="navbarLogo">
+                    Entrepedia
+                </button>
+            </a>
+            <form action="" method="POST" class="navbarSearchGroup">
+                @csrf
+                <input type="search" class="navbarSearch" placeholder="Find your interest...">
+                <button class="navbarSubmitButton" type="submit">
+                    Search
+                </button>
+            </form>
+        
+        </div>
+        <div class="navbarRight">
+            {{-- Cuma muncul kalo author --}}
+            <a href="register" class="navbarLeft">
+                <button class="navbarButton">
+                    Register
+                </button>
+            </a>
+
+            <a href="login" class="navbarLeft">
+                <button class="navbarButton">
+                    Login
+                </button>
+            </a>
+        </div>
+        
+    </div>
+</body>
+</html>
