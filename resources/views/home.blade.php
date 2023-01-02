119337e58e9d867e89ec789b7243f01b69536658
Ignatius Filbert Sevilen <sevilenfilbert@gmail.com>
2023-01-02 17:11:35 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,20 @@ Home
 @section('css')
     <link rel="stylesheet" href="/storage/css/main.css">
     <link rel="stylesheet" href="/storage/css/home.css">
+    <link rel="stylesheet" href="/storage/css/navbar.css">
     <title>Home</title>
 @endsection
 @section('content')
     <div class="bodyContainer">
         
         <div class="homeContainer">
+            <form action="" method="POST" class="navbarSearchGroup">
+                @csrf
+                <input type="search" class="navbarSearch" placeholder="Find your interest...">
+                <button class="navbarSubmitButton" type="submit">
+                    Search
+                </button>
+            </form>
             <h1 class="textHeader">Headline News</h1>
             {{-- Load Headline --}}
             @include('component.headline')
