43e4026da11af72123cece29fe3e95fb5eb47542
bryanmcl <bryanmcl0209@gmail.com>
2023-01-03 21:49:40 +0700

    create author home page, edit, and add article

---
+++ resources/views/component/author_news_card.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/storage/css/main.css">
+    <link rel="stylesheet" href="/storage/css/news.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="card" style="width: 18rem;">
+        <img src="https://picsum.photos/id/237/200/300" height="300px" class="card-img-top object-fit-cover"
+            alt="...">
+        <div class="card-body">
+            <h5 class="card-title">News Name</h5>
+            <p class="card-text text-truncate d-block">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum, amet
+                debitis earum nemo eius ipsam perferendis officia quasi perspiciatis consequuntur vero veritatis
+                adipisci corrupti culpa at cupiditate sapiente omnis aspernatur.
+            </p>
+            <p class="card-text text-secondary">
+                Created on DD-MM-YYYY
+            </p>
+            <a href="#" class="btn btn-primary text-white">See Details</a>
+            <a href="#" class="btn btn-warning text-white">Edit</a>
+            <a href="#" class="btn btn-danger text-white">Delete</a>
+        </div>
+    </div>
+</body>
+
+</html>
